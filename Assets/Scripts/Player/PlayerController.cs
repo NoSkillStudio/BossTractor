8b7701899aa7da6fb2baa1cec1c72b2fd136efcf
Maksim <sazanovichmaksim@gmail.com>
2023-05-27 15:01:11 +0300

    Добавил управление игрока (у меня некорректно работает)

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Vector2 _movement;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movement.normalized * _speed * Time.fixedDeltaTime);
+    }
+}
