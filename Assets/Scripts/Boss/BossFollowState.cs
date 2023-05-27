5e845efcf9f1b07e1dd3ebe7ad15f415aec83aaa
Maksim <sazanovichmaksim@gmail.com>
2023-05-27 20:01:07 +0300

    сделал поворот босса к игроку или таргет позиции

--- Assets/Scripts/Boss/BossFollowState.cs
+++ Assets/Scripts/Boss/BossFollowState.cs
@@ -7,13 +7,17 @@ public class BossFollowState : BossBaseState
 {
     [SerializeField] private Transform playerPosition;
     [SerializeField] private float speed = 1f;
-
+    private SpriteRenderer spriteRenderer;
     public override void EnterState(BossStateManager boss)
     {
-       
+       spriteRenderer = GetComponent<SpriteRenderer>();
     }
     public override void UpdateState(BossStateManager boss)
     {
+        if (playerPosition.position.x >= transform.position.x)
+            spriteRenderer.flipX = false;
+        else spriteRenderer.flipX = true;
+
         transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, speed * Time.fixedDeltaTime);
     }
 
--- Assets/Scripts/Boss/BossRamState.cs
+++ Assets/Scripts/Boss/BossRamState.cs
@@ -8,14 +8,19 @@ public class BossRamState : BossBaseState
 	[SerializeField] private Transform playerPosition;
     private Vector3 targetPosition;
     [SerializeField] private float speed = 1f;
-
-	public override void EnterState(BossStateManager boss)
+    private SpriteRenderer spriteRenderer;
+    public override void EnterState(BossStateManager boss)
 	{
 		//TODO: анимация обнаружения
 		targetPosition = playerPosition.position;
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	public override void UpdateState(BossStateManager boss)
 	{
+        if (playerPosition.position.x >= transform.position.x)
+            spriteRenderer.flipX = false;
+        else spriteRenderer.flipX = true;
+
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.fixedDeltaTime);
     }
 
